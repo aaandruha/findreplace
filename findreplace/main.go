23624d81c2e76725d5c72e3db882cc8f9ff61679
mindbrod <Glybin.AV@gmail.com>
2020-11-25 12:55:08 +0300

    integrate urfave/cli

--- findreplace/main.go
+++ findreplace/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 
@@ -8,7 +9,39 @@ import (
 )
 
 func main() {
-	err := cli.NewApp().Run(os.Args)
+	app := cli.NewApp()
+	app.Name = "Find and replace string in file(s) or stdin"
+	app.Usage = "Simple tool for find and replace"
+
+	myFlags := []cli.Flag{
+		&cli.StringFlag{
+			Name:  "str",
+			Value: "The quick brown fox jumps over the lazy dog",
+		},
+	}
+
+	app.Commands = []*cli.Command{
+		{
+			Name:  "find",
+			Usage: "find str in stream",
+			Flags: myFlags,
+			Action: func(c *cli.Context) error {
+				fmt.Println(c.String("str"))
+				return nil
+			},
+		},
+		{
+			Name:  "replace",
+			Usage: "find str in stream",
+			Flags: myFlags,
+			Action: func(c *cli.Context) error {
+				fmt.Println(c.String("str"))
+				return nil
+			},
+		},
+	}
+
+	err := app.Run(os.Args)
 	if err != nil {
 		log.Fatal(err)
 	}
